b774019fae70d51e7c816a8fc28e08509b0d8e07
Oliver Bennett <Oliver.Bennett@education.vic.gov.au>
2021-02-25 21:13:48 +1100

    atm

--- atm.js
+++ atm.js
@@ -3,21 +3,21 @@ process.stdin.setEncoding('utf8');
 
 // your code goes here
 let data = "";
+
 process.stdin.on('data', function(chunk) {
-  data += chunk.toString();
+    data += chunk.toString();
 });
+
 process.stdin.on('end', function() {
-    main();
+    runTestCase();
 });
-function main() {
+
+function runTestCase() {
     let lines = data.split(" ");
     const withdrawal = parseInt(lines[0]);
-    const balance = parseFloat(lines[1]);
-    const fee = parseFloat(0.50);
+    const total = parseFloat(lines[1]);
+    const charge = parseFloat(0.50);
 
-    if (withdrawal % 5 !== 0 || withdrawal > (balance+fee)) {
-        console.log(balance);
-    } else if (withdrawal % 5 === 0){
-        console.log(balance-withdrawal-fee);
-    }
+    if((withdrawal % 5 !==0) || (withdrawal+charge > total)) console.log(total);
+    else if(withdrawal % 5 === 0) console.log(total-withdrawal-charge);
 }
