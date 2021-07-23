05d63847387826bc70bfd32275a2370c1e6699da
Oliver <ocben1@gmail.com>
2021-07-24 08:20:09 +1000

    FLOW006

--- FLOW006.cpp
+++ FLOW006.cpp
@@ -1,1 +1,18 @@
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, n, sum;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        sum = 0;
+        while (n) {
+            sum += n % 10;
+            n /= 10;
+        }
+        cout << sum << endl; 
+    }
+    return 0;
+}
