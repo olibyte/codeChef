db56262f85ba90b7d8e53f067fb40ce6bd5f69db
Oliver <ocben1@gmail.com>
2021-07-24 10:12:53 +1000

    FLOW006 comments

--- FLOW006.cpp
+++ FLOW006.cpp
@@ -5,13 +5,19 @@ int main() {
 	// your code goes here
     int t, n, sum;
     cin >> t;
+    //while there's test cases
     while (t--) {
+        //read in the integer
         cin >> n;
         sum = 0;
+        //while there's a digit
         while (n) {
+            //use modulo to get last digit, add it to the sum
             sum += n % 10;
+            //perform integer division on n so we're looking at the next digit
             n /= 10;
         }
+        //print the sum on a new line
         cout << sum << endl; 
     }
     return 0;
