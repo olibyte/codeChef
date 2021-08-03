21e323ca5be0177a799746aacaf0694ab1563506
Oliver <ocben1@gmail.com>
2021-08-04 08:07:56 +1000

    FLOW007

--- FLOW007.cpp
+++ FLOW007.cpp
@@ -3,5 +3,23 @@ using namespace std;
 
 int main() {
 	// your code goes here
-	return 0;
+	int t, num, rem;
+    int rev;
+    cin>>t;
+
+    while (t--)
+    {
+        cin>>num;
+        if(num == 0) {
+            cout<<0<<endl;
+            continue;
+        }
+        
+        while (num) {
+            rev = rev * 10 + (num % 10);
+            num /= 10;
+        }
+        cout<<rev<<endl;
+        rev = 0;
+    }
 }
